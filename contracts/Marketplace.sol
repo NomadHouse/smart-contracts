14348592999165baa8b60f562f0094dad0211d60
N00bBot <n00bb0t@protonmail.com>
2022-05-03 11:19:22 -0600

    Refactoring marketplace contract

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,7 +1,5 @@
-// contracts/NFTMarketplace.sol
+// contracts/Marketplace.sol
 // SPDX-License-Identifier: MIT OR Apache-2.0
-
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
@@ -9,17 +7,45 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
-import "./interfaces/IMarketplace.sol";
-
 contract Marketplace is ReentrancyGuard {
   using Counters for Counters.Counter;
 
-  address payable public marketowner;
+  address payable public marketOwner;
+  address nftContract;
+
+  enum MarketItemState { Listed, Release, Inactive }
+
+  struct MarketItem {
+    uint id;
+    uint256 tokenId;
+    address payable seller;
+    address payable buyer;
+    uint256 price;
+    MarketItemState state;
+  }
+
+  event MarketItemListed (
+    uint indexed id,
+    uint256 indexed tokenId,
+    uint256 price,
+    MarketItemState state
+  );
+
+  event MarketItemSold (
+    uint indexed id,
+    uint256 indexed tokenId,
+    address seller,
+    address buyer,
+    uint256 price,
+    MarketItemState state
+  );
+
 
   mapping(uint256 => MarketItem) private marketItems;
 
   constructor() {
-    marketowner = payable(msg.sender);
+    marketOwner = payable(msg.sender);
+    nftContract = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8; //replace this with Collection.sol address
   }
 
 }

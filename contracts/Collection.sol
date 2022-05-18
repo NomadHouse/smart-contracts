d85fc0e82e2d45001ea7a2c27e7bf4445c28fee7
N00bBot <n00bb0t@protonmail.com>
2022-05-17 20:04:56 -0600

    temp commit

--- contracts/Collection.sol
+++ contracts/Collection.sol
@@ -1,8 +1,9 @@
 // contracts/Collection.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.4;
 
 import "@chainlink/contracts/src/v0.7/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -23,22 +24,30 @@ contract Collection is
     //=============================== STORAGE ===================================//
 
     Counters.Counter private _deedIds;
+
+    // Global variable used to store chainlinks per-title response when verifying title ownership
+    mapping(bytes32 => bool) _verifiedTitles;
+
+    // Mapping of authorized wallets that have been KYC'd
+    mapping(address => bool) _verifiedAddresses;
     
     // Params for Chainlink Request
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
 
-    // Internal Struct for tracking Fractional Deeds
+    // Internal Struct for tracking Fractional Property Deeds
     struct FractionalDeed {
       uint id;
       bytes32 titleId;
       address payable owner;
-      bool verified;
     }
 
-    // Contract base URI string
-    string internal globalTokenURI;
+    // Token base URI string 
+    string internal baseTokenUri;
+
+    // Title search api base URI string 
+    string internal titleSearchUri;
         
     // Key value pair containing a list of deeds in the contract
     mapping(uint256 => FractionalDeed) internal deeds;
@@ -72,6 +81,7 @@ contract Collection is
 
     constructor() ERC721("NomadHouse", "NMH") {
       setPublicChainlinkToken();
+      titleSearchUri = "https://bafybeihuftdtf5rjkep52k5afrydtlo4mvznafhtmrsqaunaninykew3qe.ipfs.dweb.link/";
       oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8; // Change this to our oracle contract address
       jobId = "d5270d1c311941d0b08bead21fea7747"; // Change this to the actual job id we use once it's built out
       fee = 0; // (Varies by network and job)
@@ -87,83 +97,124 @@ contract Collection is
       _unpause();
     }
 
-    function setURI(string memory newURI) external onlyOwner {
-      globalTokenURI = newURI;
+    /**
+      * @dev sets base uri for all deeds
+    */
+    function setTokenURI(string memory newURI) external whenNotPaused onlyOwner {
+      baseTokenUri = newURI;
+    }
+
+    /**
+      * @dev sets title search uri for title verification
+    */
+    function setTitleSearchURI(string memory newURI) external whenNotPaused onlyOwner {
+      titleSearchUri = newURI;
+    }
+
+    /**
+      * @dev add recipient addresses to the authorized wallets list and grant them the ability to mint
+    */
+    function authorizeWallet(address _wallet) external whenNotPaused onlyOwner {
+      require(_wallet != address(0), "Wallet address cannot be zero address");
+      _verifiedAddresses[_wallet] = true;
+    }
+
+    /**
+      * @dev remove recipient addresses to the authorized wallets list and revoke their ability to mint
+    */
+    function deauthorizeWallet(address _wallet) external whenNotPaused onlyOwner {
+      require(_wallet != address(0), "Wallet address cannot empty");
+      _verifiedAddresses[_wallet] = false;
+    }
+
+    //============================= MODIFIERS ===============================//
+
+    modifier onlyVerifiedAddress(address _address) {
+      require(_verifiedAddresses[_address] == true);
+      _;
     }
 
     //============================= PUBLIC / EXTERNAL FUNCTIONS ===============================//
 
     //================== STATE ALTERING FUNCTIONS ==================//
 
-
-
-    function verifyTitleOwnership(bytes32 titleId) public returns(bytes32 requestId) {
+     /**
+      * @dev Makes an API call to our in house title search API which will have information on the title
+      * and it's whether it's been marked as verified.
+      * @param _titleId The primary identifier for performing title searches
+    */
+    function verifyTitleOwnership(bytes32 _titleId) public returns(bytes32 requestId) {
+      require(titleSearchUri.length != 0, 'Cannot execute ChainLink request: Title Search URI is empty');
       Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
         
       // Set the URL to perform the GET request on
-      request.add("get", "https://some-title-search-api.com/");
+      request.add("get", titleSearchURI);
       
       // Set the path to find the desired data in the API response, where the response format is:
       // {"TITLEID":
       //   {
-      //    "OWNER": "JOHN DOE"
       //    "VERIFIED": true,
       //   }
       //  }
       
-      request.add("path", string(abi.encodePacked(titleId, ",OWNER,VERIFIED"))); // Chainlink nodes 1.0.0 and later support this format
+      request.add("titleId", _titleId); // Chainlink nodes 1.0.0 and later support this format
       
       // Sends the request
       return sendChainlinkRequestTo(oracle, request, fee);
     }
 
     /**
-     * Receive the response in the form of uint256
+     * Receive the response in the form of a boolean
      */ 
-    function fulfill(bytes32 _requestId, uint256 _verified) public recordChainlinkFulfillment(_requestId) {
-        verified = _verified;
+    function fulfillTitleOwnership(bytes32 _requestId, bytes32 titleId, bool _verified) public recordChainlinkFullfillment(_requestId) {
+        _verifiedTitles[titleId] = _verified;
+    }
+
+    function withdrawLink() public onlyOwner {
+      LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+      require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
     }
 
 
     function mintFractionalDeed(
-      address account,
+      address _address,
       bytes32 titleId,
       bytes memory data
-    ) external whenNotPaused {
+    ) external whenNotPaused onlyVerifiedAddress(_address) {
       uint256 currentDeedId = _deedIds.current();
       require(deeds[currentDeedId] == 0, 'Deed has already been minted');
 
-      bool titleVerified = verifyTitleOwnership(titleId);
-      require(titleVerified == true, 'Title ownership has not been verified');
+      verifyTitleOwnership(titleId);
+      require(_verifiedTitles[titleId] == true, 'Title ownership has not been verified');
 
-      _safeMint(account, currentDeedId);
+      _safeMint(_address, currentDeedId);
       deeds[currentDeedId] = FractionalDeed(
         currentDeedId,
-        account,
-        isVerified
+        _address,
+        _currentTitleVerified
       );
-      _owners[currentDeedId] = account;
+      _owners[currentDeedId] = _address;
       _deedIds.increment();
 
-      emit DeedsMinted(account, currentDeedId);
+      emit DeedsMinted(_address, currentDeedId);
     }
 
 
     /**
       * @dev Deed batch minting functionality
-      * @param account the account to mint the Deeds to
+      * @param _address the _address to mint the Deeds to
       * @param amount the amount of deeds to mint
       * @param titleId the 
       * @param data the 
       * @return uri string of the deedId metadata
      */
     function batchMintFractionalDeeds(
-      address account,
+      address _address,
       uint memory amount,
       bytes32 titleId,
       bytes memory data
-    ) external whenNotPaused {
-      uint[] tempDeedIds;
+    ) external whenNotPaused onlyVerifiedAddress(_address) {
+      uint[] memory tempDeedIds;
       uint256 currentDeedId = _deedIds.current();
       tempDeedIds.push(currentDeedId);
 
@@ -173,29 +224,29 @@ contract Collection is
         tempDeedIds.push(currentDeedId + i);
       }
 
-      bool titleVerified = verifyTitleOwnership(titleId);
-      require(titleVerified == true, 'Title ownership has not been verified');
+      verifyTitleOwnership(titleId);
+      require(_verifiedTitles[titleId] == true, 'Title ownership has not been verified');
       
       // Minting deeds through a loop
       for (uint i = 0; i < amount; i++) {
         require(_deedIds[tempDeedIds[i]] == 0, 'Deed has already been minted');
 
-        _safeMint(account, tempDeedIds[i]);
+        _safeMint(_address, tempDeedIds[i]);
+
+        // Returning array to default values (saves gas)
+        delete tempDeedIds;
 
         deeds[deedId] = FractionalDeed(
           currentDeedId,
-          account,
-          titleVerified
+          _address,
+          _currentTitleVerified
         );
-        _owners[deedId] = account;
+        _owners[deedId] = _address;
 
         _deedIds.increment();
       }
 
-      // Returning array to default values (saves gas)
-      delete tempDeedIds;
-
-      emit DeedsMinted(account, tempDeedIds);
+      emit DeedsMinted(_address, tempDeedIds);
     }
 
 
@@ -229,6 +280,42 @@ contract Collection is
     return string(abi.encodePacked(_baseURI(), Strings.toString(deedId), ".json"));
   }
 
+  /**
+    * @dev transfer function (Individual)
+    * @param from address string to send the tokens from
+    * @param to address string to send the tokens to
+    * @param deedId deed token id to transfer
+    * @param data additional data in case the minter is a contract
+  **/
+  function safeTransferFrom(
+    address from, 
+    address to, 
+    uint256 deedId,
+    bytes memory data
+  ) public virtual override onlyVerifiedAddress(_to) {
+    super.safeTransferFrom(from, to, deedId, data);
+    _owners[deedId] = to;
+  }
+
+  /**
+    * @dev transfer function (Batch)
+    * @param from address string to send the tokens from
+    * @param to address string to send the tokens to
+    * @param deedIds deed token ids to transfer
+    * @param data additional data in case the minter is a contract
+  **/
+  function safeBatchTransferFrom(
+    address from, 
+    address to, 
+    uint256[] memory deedIds, 
+    bytes memory data
+  ) public virtual onlyVerifiedAddress(_to) {
+    for (uint i=0; i < deedIds.length; i++) {
+      super.safeTransferFrom(from, to, deedIds[i], data);
+      _owners[deedIds[i]] = to;
+    }
+  }
+
   //============================== PRIVATE / INTERNAL FUNCTIONS  ==============================//
 
   //================== NON-MODIFYING FUNCTIONS ==================//
@@ -237,7 +324,7 @@ contract Collection is
     * @dev returns the metadata base uri string for all tokens
   */
   function _baseURI() internal view returns (string memory) {
-    return globalTokenURI;
+    return baseTokenUri;
   }
   
 }

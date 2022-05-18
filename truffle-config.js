d5c33ebb27ceb970d701727243877024bcba6ab2
N00bBot <n00bb0t@protonmail.com>
2022-05-17 20:04:56 -0600

    temp commit

--- truffle-config.js
+++ truffle-config.js
@@ -23,6 +23,12 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraProjectId = process.env.INFURA_KEY;
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".mnemonic").toString().trim();
+
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -34,7 +40,34 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
-  networks: {
+   plugins: ["truffle-contract-size", 'truffle-plugin-verify'],
+ 
+   networks: {
+     // Useful for testing. The `development` name is special - truffle uses it by default
+     // if it's defined here and no other network is specified at the command line.
+     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+     // tab if you use this network and you must also set the `host`, `port` and `network_id`
+     // options below to some value.
+     //
+     development: {
+       host: "127.0.0.1",     // Localhost (default: none)
+       port: 8545,            // Standard Ethereum port (default: none)
+       network_id: "*",       // Any network (default: none)
+       accounts: 5,
+       defaultEtherBalance: 500,
+       blockTime: 3,
+       skipDryRun: true
+     },
+     ganache: {
+       host: "127.0.0.1",     // Localhost (default: none)
+       port: 7545,            // Standard Ethereum port (default: none)
+       network_id: "*",       // Any network (default: none)
+       accounts: 5,
+       gasPrice: 150000000000,
+       defaultEtherBalance: 500,
+       blockTime: 3,
+       skipDryRun: true
+     },
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
@@ -71,6 +104,10 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${infuraProjectId}`, 5),
+      network_id: 42
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
